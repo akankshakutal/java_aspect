37158fc70cb3b0657b2cceaeebc466aba36548f7
akankshakutal <44019583+akankshakutal@users.noreply.github.com>
2019-08-27 07:41:14 +0530

    Initial Commit

---
+++ src/main/java/com/aspect/EmployeeServiceAspect.java
@@ -0,0 +1,22 @@
+package com.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class EmployeeServiceAspect {
+
+    @Before(value = "execution(* com.service.EmployeeService.*(..)) and args(name,empId)")
+    public void beforeAdvice(JoinPoint joinPoint, String name, String empId) {
+        System.out.println("Before method:" + joinPoint.getSignature());
+    }
+
+    @After(value = "execution(* com.service.EmployeeService.*(..)) and args(name,empId)")
+    public void afterAdvice(JoinPoint joinPoint, String name, String empId) {
+        System.out.println("After method:" + joinPoint.getSignature());
+    }
+}
