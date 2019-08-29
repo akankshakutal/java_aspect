fb9a102bcd7e11a2386d227c71114cd1f4512aea
akankshakutal <44019583+akankshakutal@users.noreply.github.com>
2019-08-29 19:12:47 +0530

    Log at controller level

--- src/main/java/com/aspect/EmployeeServiceAspect.java
+++ src/main/java/com/aspect/EmployeeServiceAspect.java
@@ -10,13 +10,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmployeeServiceAspect {
 
-    @Before(value = "execution(* com.service.EmployeeService.*(..)) and args(name,empId)")
-    public void beforeAdvice(JoinPoint joinPoint, String name, String empId) {
-        System.out.println("Before method:" + joinPoint.getSignature());
+    @Before(value = "execution(* com.Controller.*.*(..))")
+    public void beforeAdvice(JoinPoint joinPoint) {
+        String param = "";
+        Object[] arguments = joinPoint.getArgs();
+        for (Object argument : arguments) {
+            param = param.concat(argument.toString()).concat(" ");
+        }
+        System.out.println("Before method:" + joinPoint.getSignature() + param);
     }
 
-    @After(value = "execution(* com.service.EmployeeService.*(..)) and args(name,empId)")
-    public void afterAdvice(JoinPoint joinPoint, String name, String empId) {
-        System.out.println("After method:" + joinPoint.getSignature());
+    @After(value = "execution(* com.service.EmployeeService.*(..))")
+    public void afterAdvice(JoinPoint joinPoint) {
+        String param = "";
+        for (int args = 0; args < joinPoint.getArgs().length; args++) {
+            param = param.concat(joinPoint.getArgs()[args].toString()).concat(" ");
+        }
+        System.out.println("After method:" + joinPoint.getSignature() + param);
     }
 }
